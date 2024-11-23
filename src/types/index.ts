72c7aac05b01639388f056061b64d88d5063aac0
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-23 16:02:07 +0600

    create interface card

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,23 @@ import { SVGProps } from "react";
 export type IconSvgProps = SVGProps<SVGSVGElement> & {
   size?: number;
 };
+
+
+export interface IPost {
+  _id: string;
+  title: string;
+  description: string;
+  images: string[];
+  location: string;
+  city: string;
+  dateFound: string;
+  status: string;
+  isReported: boolean;
+  reportCount: number;
+  category: string;
+  user: string;
+  questions: string[];
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
