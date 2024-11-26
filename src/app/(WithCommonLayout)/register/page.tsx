5389cd4e76a62266ab498dde0c235b507e4952f6
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-26 15:25:07 +0600

    create auth service and axious

--- src/app/(WithCommonLayout)/register/page.tsx
+++ src/app/(WithCommonLayout)/register/page.tsx
@@ -1,13 +1,41 @@
 "use client";
 
 import FXForm from "@/src/components/form/FXForm";
+import FXInput from "@/src/components/form/FXInput";
+import registerValidationSchema from "@/src/schemas/register.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@nextui-org/button";
+import Link from "next/link";
+import { FieldValues, SubmitHandler } from "react-hook-form";
 
 
 
 
 export default function RegisterPage() {
-  
+  // const { mutate: handleUserRegistration, isPending } = useUserRegistration();
+
+  //   useEffect(() => {
+  //     if (isPending) {
+  //       // Handle Loading satate
+  //     }
+  //   }, [isPending]);
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    const userData = {
+      ...data,
+      profilePhoto:
+        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+    };
+
+    console.log("Inside form user data: ", userData);
+
+    handleUserRegistration(userData);
+  };
+
+  if (isPending) {
+    //  handle loading state
+  }
+
   return (
     <div className="flex h-[calc(100vh-100px)] flex-col items-center justify-center">
       <h3 className="my-2 text-xl font-bold">Register with FoundX</h3>
@@ -22,6 +50,7 @@ export default function RegisterPage() {
             password: "123456",
           }}
           resolver={zodResolver(registerValidationSchema)}
+          onSubmit={onSubmit}
         >
           <div className="py-3">
             <FXInput label="Name" name="name" size="sm" />
