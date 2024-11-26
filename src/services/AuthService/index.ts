b61435ef093342073cbadc7d593561c485ffa36d
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-26 15:53:10 +0600

    create register login

---
+++ src/services/AuthService/index.ts
@@ -0,0 +1,9 @@
+import { FieldValues } from "react-hook-form";
+
+export const registerUser = async (userData: FieldValues) => {
+    try {
+     
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  };
