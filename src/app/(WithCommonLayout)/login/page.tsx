75ffda4041f4aa87766ab71c5a4d0e85359b2ca1
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-28 01:14:59 +0600

    create set user

--- src/app/(WithCommonLayout)/login/page.tsx
+++ src/app/(WithCommonLayout)/login/page.tsx
@@ -7,11 +7,33 @@ import FXForm from "@/src/components/form/FXForm";
 import FXInput from "@/src/components/form/FXInput";
 import loginValidationSchema from "@/src/schemas/logoin.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useUserLogin } from "@/src/hooks/auth.hook";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const LoginPage = () => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const { setIsLoading: userLoading } = useUser();
+
+  const redirect = searchParams.get("redirect");
+
+  const { mutate: handleUserLogin, isPending, isSuccess } = useUserLogin();
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    console.log(data);
+    handleUserLogin(data);
+    userLoading(true);
   };
+
+  useEffect(() => {
+    if (!isPending && isSuccess) {
+      if (redirect) {
+        router.push(redirect);
+      } else {
+        router.push("/");
+      }
+    }
+  }, [isPending, isSuccess]);
 
   return (
     <div className="flex h-[calc(100vh-100px)] w-full flex-col items-center justify-center">
