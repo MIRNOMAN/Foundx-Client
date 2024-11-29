ba9ff6c8f00fcc67533a5e18b3621c99682f7f4c
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-11-30 00:32:01 +0600

    create user info

--- src/context/user.provider.tsx
+++ src/context/user.provider.tsx
@@ -1,3 +1,26 @@
+import {
+    createContext,
+    Dispatch,
+    ReactNode,
+    SetStateAction,
+    useContext,
+    useEffect,
+    useState,
+  } from "react";
+  import { IUser } from "../types";
+  import { getCurrentUser } from "../services/AuthService";
+  
+  const UserContext = createContext<IUserProviderValues | undefined>(undefined);
+  
+  interface IUserProviderValues {
+    user: IUser | null;
+    isLoading: boolean;
+    setUser: (user: IUser | null) => void;
+    setIsLoading: Dispatch<SetStateAction<boolean>>;
+  }
+
+
+
 const UserProvider = ({ children }: { children: ReactNode }) => {
     const [user, setUser] = useState<IUser | null>(null);
     const [isLoading, setIsLoading] = useState(true);
@@ -19,3 +42,16 @@ const UserProvider = ({ children }: { children: ReactNode }) => {
       </UserContext.Provider>
     );
   };
+
+
+  export const useUser = () => {
+    const context = useContext(UserContext);
+  
+    if (context === undefined) {
+      throw new Error("useUser must be used within the UserProvider context");
+    }
+  
+    return context;
+  };
+  
+  export default UserProvider;
