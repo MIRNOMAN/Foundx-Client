d132b9eb07ebec30b7760406645103ded1b478dd
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-12-03 00:39:38 +0600

    create data post

--- src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
+++ src/app/(WithCommonLayout)/(user)/profile/create-post/page.tsx
@@ -1,4 +1,18 @@
-import { FieldValues, SubmitHandler, useFieldArray, useForm } from "react-hook-form";
+"use client";
+
+import FXInput from "@/src/components/form/FXInput";
+
+import { Divider } from "@nextui-org/divider";
+
+import { Button } from "@nextui-org/button";
+
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useFieldArray,
+  useForm,
+} from "react-hook-form";
 
 export default function page() {
   
@@ -26,7 +40,29 @@ export default function page() {
   
     return (
       <div>
-         <h1>User create post page</h1>
-      </div>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FXInput name="title" label="Title" />
+
+          <Divider className="my-5" />
+
+          <div className="flex justify-between items-center">
+            <h1 className="text-xl">Owner verification questions</h1>
+            <Button onClick={() => handleFieldAppend()}>Append</Button>
+          </div>
+
+          {fields.map((field, index) => (
+            <div key={field.id} className="flex items-center">
+              <FXInput name={`questions.${index}.value`} label="Question" />
+              <Button onClick={() => remove(index)}>Remove</Button>
+            </div>
+          ))}
+
+          <Divider className="my-5" />
+
+          <Button type="submit">Post</Button>
+        </form>
+      </FormProvider>
+    </div>
     );
   }
