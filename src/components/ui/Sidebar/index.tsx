e0edd3a469efe40532c49225c952493f85a680f2
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-12-03 20:12:44 +0600

    update user

--- src/components/ui/Sidebar/index.tsx
+++ src/components/ui/Sidebar/index.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { adminLinks, userLinks } from "./constants";
 import { useUser } from "@/src/context/user.provider";
 import { SidebarOptions } from "./SidebarOptions";
+import Image from "next/image";
 
 
 const Sidebar = () => {
@@ -13,11 +14,16 @@ const Sidebar = () => {
     <div>
       <div className="rounded-xl bg-default-100 p-2">
         <div className="h-[330px] w-full rounded-md">
-          <h1>Ekhane user er profile picture hobe</h1>
+        <Image
+            alt="profile"
+            height={100}
+            src={user?.profilePhoto as string}
+            width={100}
+          />
         </div>
         <div className="my-3">
-          <h1 className="text-2xl font-semibold">Mofiz</h1>
-          <p className="break-words text-sm">mofiz@gmail.com</p>
+          <h1 className="text-2xl font-semibold">{user?.name}</h1>
+          <p className="break-words text-sm">{user?.email}</p>
         </div>
         <Button
           as={Link}
