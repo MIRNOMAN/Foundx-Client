b7e3e8c10b29dbf50a592c9ae27458de380593fa
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-12-08 23:14:51 +0600

    create found item post

--- src/app/(WithCommonLayout)/found-items/page.tsx
+++ src/app/(WithCommonLayout)/found-items/page.tsx
@@ -1,5 +1,5 @@
-import Container from "@/src/components/UI/Container";
-import Post from "@/src/components/UI/Post";
+import Container from "@/src/components/ui/container";
+import Post from "@/src/components/ui/Post";
 import axiosInstance from "@/src/lib/AxiosInstance";
 import { IPost } from "@/src/types";
 
