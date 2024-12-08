f9efc5965d3abc04d1549d814c1fafd76524b9a5
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-12-08 23:23:18 +0600

    craete image gallery

---
+++ src/components/ui/Post/ImageGallery.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import LightGallery from "lightgallery/react";
+
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+
+import Image from "next/image";
+import Link from "next/link";
+
+interface IProps {
+  images: string[];
+}
