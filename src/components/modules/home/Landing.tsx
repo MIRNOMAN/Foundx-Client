1fa6dcb83d8ba3cacc827c4a396e2f8423048ab3
MIRNOMAN <abdullahalnoman1509@gmail.com>
2024-12-24 23:48:26 +0600

    update landing page

--- src/components/modules/home/Landing.tsx
+++ src/components/modules/home/Landing.tsx
@@ -1,9 +1,10 @@
-import { Input } from "@nextui-org/input";
+"use client"
 
+import { Input } from "@nextui-org/input";
 import { SearchIcon } from "../../icons";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation"; 
 import { ISearchResult } from "@/src/types";
 import Link from "next/link";
 import useDebounce from "@/src/hooks/debounce.hook";
@@ -44,63 +45,63 @@ export default function Landing() {
 
   return (
     <div className="h-[calc(100vh-64px)] bg-[url('/glass.jpg')] bg-cover bg-center">
-    <div className="pt-32 max-w-xl flex-1 mx-auto">
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex-1">
-          <Input
-            {...register("search")}
-            aria-label="Search"
-            classNames={{
-              inputWrapper: "bg-default-100",
-              input: "text-sm",
-            }}
-            placeholder="Search..."
-            size="lg"
-            startContent={
-              <SearchIcon className="pointer-events-none flex-shrink-0 text-base text-default-400" />
-            }
-            type="text"
-          />
-        </div>
-      </form>
-      {searchResults.length > 0 && (
-        <div className="mt-2 rounded-xl bg-default-100 p-3">
-          <div className="space-y-3">
-            {searchResults.map((item, index) => (
-              <Link
-                key={index}
-                className="text-default-900 block rounded-md from-default-200 p-2 transition-all hover:bg-gradient-to-l"
-                href={`/found-items/${item.id}`}
-              >
-                <div>
-                  <div className="flex items-center gap-2">
-                    <img
-                      alt="item"
-                      className="h-20 w-20 rounded-md"
-                      src={item.thumbnail}
-                    />
-                    <div>
-                      <p className="text-lg font-semibold">{item.title}</p>
-                      <p className="mt-1 line-clamp-2 h-12 w-full text-sm">
-                        {item.description}
-                      </p>
+      <div className="pt-32 max-w-xl flex-1 mx-auto">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex-1">
+            <Input
+              {...register("search")}
+              aria-label="Search"
+              classNames={{
+                inputWrapper: "bg-default-100",
+                input: "text-sm",
+              }}
+              placeholder="Search..."
+              size="lg"
+              startContent={
+                <SearchIcon className="pointer-events-none flex-shrink-0 text-base text-default-400" />
+              }
+              type="text"
+            />
+          </div>
+        </form>
+        {searchResults.length > 0 && (
+          <div className="mt-2 rounded-xl bg-default-100 p-3">
+            <div className="space-y-3">
+              {searchResults.map((item, index) => (
+                <Link
+                  key={index}
+                  className="text-default-900 block rounded-md from-default-200 p-2 transition-all hover:bg-gradient-to-l"
+                  href={`/found-items/${item.id}`}
+                >
+                  <div>
+                    <div className="flex items-center gap-2">
+                      <img
+                        alt="item"
+                        className="h-20 w-20 rounded-md"
+                        src={item.thumbnail}
+                      />
+                      <div>
+                        <p className="text-lg font-semibold">{item.title}</p>
+                        <p className="mt-1 line-clamp-2 h-12 w-full text-sm">
+                          {item.description}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </Link>
-            ))}
+                </Link>
+              ))}
+            </div>
+            <div className="mt-3 flex justify-center border-t-1 border-default-50 pt-3">
+              <button
+                className="flex items-center justify-center gap-1"
+                onClick={() => handleSeeAll(searchTerm)}
+              >
+                <span>See All</span>
+              </button>
+            </div>
           </div>
-          <div className="mt-3 flex justify-center border-t-1 border-default-50 pt-3">
-            <button
-              className="flex items-center justify-center gap-1"
-              onClick={() => handleSeeAll(searchTerm)}
-            >
-              <span>See All</span>
-            </button>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
-  </div>
   );
 }
